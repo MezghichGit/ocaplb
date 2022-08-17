075d1fc16c570a4de93910e1b41bb13ac227f9de
moahmed amine mezghich <git@mezghich.com>
2022-08-17 10:07:13 +0100

    les types primitifs entiers

--- src/chapter1/Test.java
+++ src/chapter1/Test.java
@@ -3,7 +3,26 @@ package chapter1;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Les 8 types primitfs");
+		//Les entiers
+		
+		byte x = 10;
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		short y = 200;
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		
+		int z = 400;
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		long v = 400;
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
 
 	}
 
