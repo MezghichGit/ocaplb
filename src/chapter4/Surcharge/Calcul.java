cf9d99879ff89f0dd8955efe69c804133da490fe
moahmed amine mezghich <git@mezghich.com>
2022-08-19 09:26:18 +0100

    surcharge de méthode

--- src/chapter4/Surcharge/Calcul.java
+++ src/chapter4/Surcharge/Calcul.java
@@ -1,10 +1,32 @@
 package chapter4.Surcharge;
 
 public class Calcul {
+	
+	//La surcharge de méthode
+	
+	public static void somme(int x, int y)
+	{
+		System.out.println("int x, int y");
+	}
+	public static void somme(short x, short y)
+	{
+		System.out.println("short x, short y");
+	}
+	public static void somme(double x, double y)
+	{
+		System.out.println("double x, double y");
+	}
+	public static void somme(Integer x, Integer y)
+	{
+		System.out.println("Integer x, Integer y");
+	}
+	public static void somme(int ...x)
+	{
+		System.out.println("int ...x");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		somme(10,20);
 	}
 
 }
